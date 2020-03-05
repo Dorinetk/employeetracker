65f6f81e350d6b5686b29fa38d4b19c8ed6fc994
Your Name Dorinetk <dketchiozo@gmail.com>
2020-03-05 15:27:39 -0500

    Database created

---
+++ seed.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+ id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(5,2),
+    department_id INTEGER NOT NULL
+    
+);
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+
+);
